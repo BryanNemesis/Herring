88cc2f0c297e3fd6ae7ff5f4522f2f2da0c03209
Igor Binkowski <igor.binkowski@nokia.com>
2021-01-28 22:21:05 +0100

    in Fillet model: fixed attribute name in serialize method, changed ordering to by id backwards.

--- fillets/models.py
+++ fillets/models.py
@@ -6,9 +6,12 @@ class Fillet(models.Model):
     text = models.TextField(blank=True, null=True)
     image = models.FileField('/images/costamn ', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return {
             'id': self.id,
             'text': self.text,
-            'likes': random.randint(0, 1000),
+            'like_count': random.randint(0, 1000),
         }
