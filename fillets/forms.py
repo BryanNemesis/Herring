cd8f83d704d7816aa25fb21d042eb55dfa61d07f
Igor Binkowski <igor.binkowski@nokia.com>
2021-01-28 22:19:45 +0100

    Added method for censoring bad words to FilletForm.

--- fillets/forms.py
+++ fillets/forms.py
@@ -4,6 +4,7 @@ from .models import Fillet
 
 
 MAX_FILLET_LENGTH = 255
+BAD_WORDS = ['kurwa', 'huj', 'dupa', 'fuck', 'shit']
 
 
 class FilletForm(forms.ModelForm):
@@ -15,4 +16,12 @@ class FilletForm(forms.ModelForm):
         text = self.cleaned_data.get('text')
         if len(text) > MAX_FILLET_LENGTH:
             raise forms.ValidationError(f"Fillets can't be longer than {MAX_FILLET_LENGTH} characters.")
+        censor_text(text)
         return text
+
+    @staticmethod
+    def censor_text(text):
+        for bad_word in BAD_WORDS:
+            if bad_word in text.lower():
+                raise forms.ValidationError(
+                    f"Fillets cannot contain forbidden word '{bad_word.title()}'. Please be polite!")
