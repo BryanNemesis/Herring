8ae64b306b54d345321f652c7e324b08a54422e7
Igor Binkowski <igor.binkowski@nokia.com>
2021-01-29 13:47:40 +0100

    Added Fillet model to admin page.

--- fillets/admin.py
+++ fillets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fillet
+
+
+class FilletAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'id', 'user']
+    search_fields = ['user__username', 'user__email']
+
+    class Meta:
+        model = Fillet
+
+
+admin.site.register(Fillet, FilletAdmin)
--- fillets/models.py
+++ fillets/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-import random
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
 
 
 class Fillet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(blank=True, null=True)
     image = models.FileField('/images/costamn ', blank=True, null=True)
+
+    def __str__(self):
+        if len(self.text) > 30:
+            return self.text[:30] + '...'
+        else:
+            return self.text
 
     class Meta:
         ordering = ['-id']
