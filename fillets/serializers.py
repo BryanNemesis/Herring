5c877b8bb42f48a05bf44e4ba81e3a50cba54d41
Igor Binkowski <igor.binkowski@nokia.com>
2021-01-31 21:41:48 +0100

    Added serializers for fillets and fillet actions

---
+++ fillets/serializers.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from rest_framework import serializers
+
+from .models import Fillet
+
+
+class FilletActionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    action = serializers.CharField()
+
+    def validate_action(self, value):
+        value = value.lower().strip()
+        if value not in settings.FILLET_ACTION_OPTIONS:
+            raise serializers.ValidationError(f"'{value}' is not a valid action for fillets")
+        return value
+
+
+class FilletSerializer(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Fillet
+        fields = ['id', 'text', 'like_count']
+
+    def get_like_count(self, obj):
+        return obj.likes.count()
+
+    def validate_text(self, value):
+        if len(value) > settings.MAX_FILLET_LENGTH:
+            raise serializers.ValidationError(
+                f"Fillets can't be longer than {settings.MAX_FILLET_LENGTH} characters.")
+        if not value.strip():
+            raise serializers.ValidationError("Fillets cannot be empty.")
+        self.censor_text(value)
+        return value
+
+    @staticmethod
+    def censor_text(text):
+        for bad_word in settings.BAD_WORDS:
+            if bad_word in text.lower():
+                raise serializers.ValidationError(
+                    f"Fillets cannot contain forbidden word '{bad_word.title()}'. Please be polite!")
