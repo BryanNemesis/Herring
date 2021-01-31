494f7bfa144805000d7e871dd5b1fbcf51be294b
Igor Binkowski <igor.binkowski@nokia.com>
2021-01-31 21:34:22 +0100

    Changed forms to use constants from settings.py

--- fillets/forms.py
+++ fillets/forms.py
@@ -1,10 +1,7 @@
 from django import forms
+from django.conf import settings
 
 from .models import Fillet
-
-
-MAX_FILLET_LENGTH = 255
-BAD_WORDS = ['kurwa', 'huj', 'dupa', 'fuck', 'shit']
 
 
 class FilletForm(forms.ModelForm):
@@ -14,14 +11,14 @@ class FilletForm(forms.ModelForm):
 
     def clean_text(self):
         text = self.cleaned_data.get('text')
-        if len(text) > MAX_FILLET_LENGTH:
-            raise forms.ValidationError(f"Fillets can't be longer than {MAX_FILLET_LENGTH} characters.")
+        if len(text) > settings.MAX_FILLET_LENGTH:
+            raise forms.ValidationError(f"Fillets can't be longer than {settings.MAX_FILLET_LENGTH} characters.")
         self.censor_text(text)
         return text
 
     @staticmethod
     def censor_text(text):
-        for bad_word in BAD_WORDS:
+        for bad_word in settings.BAD_WORDS:
             if bad_word in text.lower():
                 raise forms.ValidationError(
                     f"Fillets cannot contain forbidden word '{bad_word.title()}'. Please be polite!")
