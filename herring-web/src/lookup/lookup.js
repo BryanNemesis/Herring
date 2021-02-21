6eae17c5330134c92007b6f1cd3a2688a5a9353f
ibinkows <igor.binkowski@nokia.com>
2021-02-21 13:46:40 +0100

    reintroduced csrf token to lookup function in react app

--- herring-web/src/lookup/lookup.js
+++ herring-web/src/lookup/lookup.js
@@ -8,35 +8,35 @@ function lookup(method, endpoint, callback, data) {
     xhr.responseType = 'json'
     xhr.open(method, url)
     xhr.setRequestHeader('Content-Type', 'application/json')
-    // const csrftoken = getCookie('csrftoken')
-    // if (csrftoken) {
-    //   xhr.setRequestHeader('X-CSRFToken', csrftoken)
-    //   xhr.setRequestHeader('HTTP_X_REQUESTED_WITH', 'XMLHttpRequest')
-    //   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-    // }
+    const csrftoken = getCookie('csrftoken')
+    if (csrftoken) {
+      xhr.setRequestHeader('X-CSRFToken', csrftoken)
+      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+    }
     xhr.onload = () => {
       callback(xhr.response, xhr.status)
     }
     xhr.onerror = (e) => {
+      console.log(e)
       callback({'message': 'Request failed'}, 400)
     }
     xhr.send(jsonData)
   }
   
-  // function getCookie(name) {
-  //   var cookieValue = null;
-  //   if (document.cookie && document.cookie !== '') {
-  //       var cookies = document.cookie.split(';');
-  //       for (var i = 0; i < cookies.length; i++) {
-  //           var cookie = cookies[i].trim();
-  //           // Does this cookie string begin with the name we want?
-  //           if (cookie.substring(0, name.length + 1) === (name + '=')) {
-  //               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-  //               break;
-  //           }
-  //       }
-  //   }
-  //   return cookieValue;
-  // }
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
 
 export default lookup
