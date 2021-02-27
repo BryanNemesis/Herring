11f3317485362c0c300519e96b87ff5418a8d0eb
ibinkows <igor.binkowski@nokia.com>
2021-02-27 14:01:47 +0100

    updated Profile model with fields related to followers

--- profiles/models.py
+++ profiles/models.py
@@ -5,10 +5,20 @@ from django.db.models.signals import post_save
 
 User = settings.AUTH_USER_MODEL
  
+class FollowerRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     country = models.CharField(max_length=50, null=True, blank=True)
     bio = models.TextField(max_length=2000, null=True, blank=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f'{self.id}: Profile of user {self.user.username}'
