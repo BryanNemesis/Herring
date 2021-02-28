d5c93c008e1b853ba6e3c859b6857df8f8134631
ibinkows <igor.binkowski@nokia.com>
2021-02-28 19:44:56 +0100

    merged follow functionality to profile detail api view

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -10,36 +10,22 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def user_follow_view(request, username):
-    me = request.user
-    other_user_qs = User.objects.filter(username=username)
-    if me.username == username:
-        my_followers = me.profile.followers.all()
-        return Response({'count': my_followers.count()}, status=200)
-    if not other_user_qs.exists():
-        return Response({}, status=404)
-    other_user = other_user_qs.first()
-    followers = other_user.profile.followers
-    data = request.data or {}
-    action = data.get('action')
-    if action == 'follow':
-        followers.add(me)
-    elif action == 'unfollow':
-        followers.remove(me)
-
-    current_followers_qs = followers.all()
-
-    return Response({'count': current_followers_qs.count()}, status=200)
     
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def profile_detail_api_view(request, username):
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
         return Response({'detail': 'User not found'}, status=404)
     profile_obj = qs.first()
+    me = request.user
 
-    data = PublicProfileSerializer(instance=profile_obj, context={'request': request})
-    return Response(data.data)
+    if request.method == 'POST' and profile_obj.user != me:
+        data = request.data or {}
+        action = data.get('action')
+        if action == 'follow':
+            profile_obj.followers.add(me)
+        elif action == 'unfollow':
+            profile_obj.followers.remove(me)
+
+    serializer = PublicProfileSerializer(instance=profile_obj, context={'request': request})
+    return Response(serializer.data)
