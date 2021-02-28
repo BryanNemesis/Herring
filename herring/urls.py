10ba014bb9bf4b90227f91a8edeb7a2978cdb31c
ibinkows <igor.binkowski@nokia.com>
2021-02-28 17:29:50 +0100

    got rid of regex urls

--- herring/urls.py
+++ herring/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -9,9 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fillets.urls')),
     path('', include('accounts.urls')),
-    re_path('profiles?/', include('profiles.urls')),
+    path('profiles/', include('profiles.urls')),
     path('api/fillets/', include('fillets.api.urls')),
-    re_path('api/profiles?/', include('profiles.api.urls')),
+    path('api/profiles/', include('profiles.api.urls')),
 
 ]
 
